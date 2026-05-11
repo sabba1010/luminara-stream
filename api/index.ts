6ce5d4aa590258b64f16d620596ce311ffa40581
Sabba Hossain <sabbahossain@gmail.com>
2026-05-11 14:50:29 +0600

    saasasd

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,14 @@
-import handler from '../dist/server/server.js';
-
 export const config = {
   runtime: 'edge',
 };
 
 export default async function (request: Request) {
-  return handler.fetch(request);
+  try {
+    // @ts-ignore
+    const handler = await import('../dist/server/server.js');
+    return handler.default.fetch(request);
+  } catch (error) {
+    console.error('Error in Vercel Edge Function:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
 }
