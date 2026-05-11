400fbbb9ad6586e855ca0c52c4fce692a542480b
Sabba Hossain <sabbahossain@gmail.com>
2026-05-11 12:54:11 +0600

    dsfsd

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+// import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
-    react(),
-    tailwindcss(),
     tanstackStart({
       server: {
         entry: "src/server.ts",
       },
     }),
+    react(),
+    tailwindcss(),
     tsconfigPaths(),
   ],
 });
